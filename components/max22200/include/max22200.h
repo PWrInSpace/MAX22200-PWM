c485a22241d85f8b057054d5111e5c21ac0288fd
chalenger15 <pawel.morawski.2006@gmail.com>
2026-07-13 13:24:41 +0200

    gpio and spi initialization

--- components/max22200/include/max22200.h
+++ components/max22200/include/max22200.h
@@ -3,8 +3,21 @@
 
 #include "esp_err.h"
 #include "driver/spi_master.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
 
-void init_max22200();
+//piny, dopasować przed uruchomieniem
+#define PIN_MISO   19
+#define PIN_MOSI   23
+#define PIN_CLK    18
+#define PIN_CS     5
+#define PIN_CMD    4
+#define PIN_ENABLE 2
+
+void max22200_init_hardware(void);
+void max22200_write(uint8_t channel, uint32_t val);
+uint32_t max22200_read(uint8_t channel);
 
 
 #endif //MAX22200_H
--- main/main.c
+++ main/main.c
@@ -1,6 +1,7 @@
 #include "max22200.h"
+#include "max22200_regs.h"
 #include "esp_log.h"
 
 void app_main(void) {
-    init_max22200();
+    max22200_init_hardware();
 }
