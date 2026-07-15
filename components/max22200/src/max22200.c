ac70000207042b8241332bcc78baa9982ad360cb
chalenger15 <pawel.morawski.2006@gmail.com>
2026-07-15 13:26:23 +0200

    channel write/read and communication tests

--- components/max22200/src/max22200.c
+++ components/max22200/src/max22200.c
@@ -31,3 +31,39 @@ void max22200_init_hardware(void) {
     };
     spi_bus_add_device(SPI3_HOST, &dev_cfg, &spi_handle);
 }
+
+void max22200_write(uint8_t channel, uint32_t val) {
+    uint8_t cmd = (1 << 7) | (channel << 1); 
+    uint8_t data[4] = { (val >> 24) & 0xFF, (val >> 16) & 0xFF, (val >> 8) & 0xFF, val & 0xFF };
+
+    gpio_set_level(PIN_CMD, 1);
+    gpio_set_level(PIN_CS, 0);
+    spi_transaction_t t1 = { .length = 8, .tx_buffer = &cmd };
+    spi_device_polling_transmit(spi_handle, &t1);
+    gpio_set_level(PIN_CS, 1);
+
+    gpio_set_level(PIN_CMD, 0);
+    gpio_set_level(PIN_CS, 0);
+    spi_transaction_t t2 = { .length = 32, .tx_buffer = data };
+    spi_device_polling_transmit(spi_handle, &t2);
+    gpio_set_level(PIN_CS, 1);
+}
+
+uint32_t max22200_read(uint8_t channel) {
+    uint8_t cmd = (channel << 1); 
+    uint8_t rx[4] = {0};
+
+    gpio_set_level(PIN_CMD, 1);
+    gpio_set_level(PIN_CS, 0);
+    spi_transaction_t t1 = { .length = 8, .tx_buffer = &cmd };
+    spi_device_polling_transmit(spi_handle, &t1);
+    gpio_set_level(PIN_CS, 1);
+
+    gpio_set_level(PIN_CMD, 0);
+    gpio_set_level(PIN_CS, 0);
+    spi_transaction_t t2 = { .length = 32, .rx_buffer = rx };
+    spi_device_polling_transmit(spi_handle, &t2);
+    gpio_set_level(PIN_CS, 1);
+
+    return ((uint32_t)rx[0] << 24) | ((uint32_t)rx[1] << 16) | ((uint32_t)rx[2] << 8) | rx[3];
+}
--- main/main.c
+++ main/main.c
@@ -3,5 +3,29 @@
 #include "esp_log.h"
 
 void app_main(void) {
+    //inicjalizacja
     max22200_init_hardware();
+    vTaskDelay(pdMS_TO_TICKS(10));
+    //test status
+    uint32_t status_val = max22200_read(MAX22200_ADDR_STATUS);
+    ESP_LOGE("ODCZYT","Odczytany STATUS: 0x%08lX\n", status_val);
+
+    if (status_val == 0x00000000 || status_val == 0xFFFFFFFF) {
+        ESP_LOGE("BLAD","STATUS niepoprawny. Sprawdź kable i zasilanie!\n");
+        return;
+    }
+    ESP_LOGE("OK","Rejestr STATUS odpowiedzial.\n");
+    //write->read-back test
+    uint32_t test_pattern = 0x2A;
+    ESP_LOGE("ZAPIS","Zapisuję do CFG_CH0: 0x%08lX\n", test_pattern);
+    max22200_write(MAX22200_ADDR_CH0, test_pattern);
+
+    uint32_t read_back = max22200_read(MAX22200_ADDR_CH0);
+    ESP_LOGE("ODCZYT","Odczytano z CFG_CH0: 0x%08lX\n", read_back);
+
+    if (read_back == test_pattern) {
+        ESP_LOGE("OK","Fizyczna warstwa SPI + CMD + zasilanie DZIALAJĄ!\n");
+    } else {
+        ESP_LOGE("BLAD","Dane w petli zostały znieksztalcone!\n");
+    }
 }
